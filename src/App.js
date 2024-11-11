536cc7ee598c7e7273fd67370ef8b14214137cc8
hawraa-3 <hawraaalmahdi.33@gmail.com>
2024-11-11 19:28:58 +0300

    part22

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 //import logo from './logo.svg';
 import './App.css';
 import storeImage from "./book.webp"
+import javabook from "./book java.jpeg"
+import cssbook from "./css.jpeg"
 
 function App() {
   console.log(storeImage)
+
+
+  const products=[
+  {
+    name:"Beginning JavaScript",
+    price:"10KD",
+
+  },
+  {
+    name:"HTML and css book ",
+    price:"10KD",
+
+  }
+  ];
+
+
+const productsList = products.map((product,index)=>{
+  return(
+    <div className="product" key={index}>
+  <img className="javaimage" src={javabook}></img>
+  <p> {product.name} </p>
+  <h6>{product.price}</h6>
+</div>
+  )
+})
   return (
+
 <div className="App">
+<img class="storeImage" src={storeImage}></img>
  <h1>Bookstore</h1>
  <p>this is my book store</p>
 
-<img class="storeImage" src={storeImage}></img>
+
+
+<div className="Parent">
+
+
+{productsList}
+
+</div>
 
     </div>
   );
