11aba66f67662f342946517938c1df0a90cb9739
hawraa-3 <hawraaalmahdi.33@gmail.com>
2024-11-14 13:31:48 +0300

    part 66

--- src/data/products.js
+++ src/data/products.js
@@ -6,11 +6,35 @@ const productsData=[
     },
     {
       name:"HTML and css book ",
-      price:"10KD",
+      price:"20KD",
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9iVT8d4Gg2vMFrsIrjpYwYBE0-9RV2XhiGg&s",
       
   
-    }
+    },
+    {
+        name:"c++ book",
+        price:"30KD",
+        image:"https://media.wiley.com/product_data/coverImage300/79/04716069/0471606979.jpg",
+        
+    
+      },
+      {
+        name:"Python book ",
+        price:"40KD",
+        image:"https://media.wiley.com/product_data/coverImage300/19/11195733/1119573319.jpg",
+        
+    
+      },
+      {
+        name:"java book ",
+        price:"50KD",
+        image:"https://in.bpbonline.com/cdn/shop/products/1021_Front_1600x.jpg?v=1625292682",
+        
+    
+      },
+
+
+
     ];
     
     
